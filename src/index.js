486982964fa7af6c385be9c49b43e8faf48a0278
Krytosys <kristen_espiritu@dlsu.edu.ph>
2020-07-21 18:22:05 +0800

    Unit test + code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 const functions = {
   gradeClassifier: (grade) => {
     // TODO: Write code here
+    if(typeof grade == 'string')
+      return "Cannot be non-integer";
+
+    else if(typeof grade == "undefined")
+      return "Cannot be null";
+
+    else if(grade < 0)
+      return "Cannot be negative";
+
+    else if(grade > 100)
+      return "Exceeded grade limit";
+
+    else{
+        grade = Math.floor(grade);
+        if(grade <= 74.99 )
+          return "D";
+        else if(grade >= 75.00 && grade <= 79.99 )
+          return "N";
+        else if(grade >= 80.00 && grade <= 84.99)
+          return "S";
+        else if(grade >= 85.00 && grade <= 89.99)
+          return "G";
+        else if(grade >= 90.00 && grade <= 94.99)
+          return "V";
+        else if(grade >= 95.00 && grade <= 100)
+          return "O";
+    }
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,224 @@
 const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
-describe('Scenario', () => {
-  it('When ..., then ...', () => {
-    // Arrange
+describe('Grade classifier', () => {
+  it('When the user inputs an integer between 95-100, then the function should an "O"/Outstanding grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(95);
 
-    // Act
+    //assert
+    expect(grade).toBe("O");
+    
+  });
+  it('When the user inputs an integer between 90-94, then the function should return a "V"/Very Good grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(90);
 
-    // Assert
+    //assert
+    expect(grade).toBe("V");
+
+  });
+  it('When the user inputs an integer between 85-89, then the function should return a "G"/Good grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(86);
+
+    //assert
+    expect(grade).toBe("G");
+
+  });
+  it('When the user inputs an integer between 80-84, then the function should return an "S"/Satisfactory grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(81);
+
+    //assert
+    expect(grade).toBe("S");
+
+  });
+  it('When the user inputs an integer between 75-79, then the function should return an "N"/Needs Improvement grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(78);
+
+    //assert
+    expect(grade).toBe("N");
+  });
+  it('When the user inputs an integer 74.99 and below, then the function should return an "D"/Did Not Meet Expectation grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(73);
+
+    //assert
+    expect(grade).toBe("D");
+  });
+
+  it('When user inputs a float between 95-100, then the function should an "O"/Outstanding grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(99.999999);
+
+    //assert
+    expect(grade).toBe("O");
+
+  });
+  
+  it('When user inputs a float between 90-94, then the function should return a "V"/Very Good grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(92.0001);
+
+    //assert
+    expect(grade).toBe("V");
+
+  });
+  
+  it('When user inputs a float between 85-89, then the function should return a "G"/Good grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(89.999);
+
+    //assert
+    expect(grade).toBe("G");
+
+  });
+  
+  it('When user inputs a float between 80-84, then the function should return an "S"/Satisfactory grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(82.293);
+
+    //assert
+    expect(grade).toBe("S");
+
+  });
+  
+  it('When user inputs a float between 75-79, then the function should return an "N"/Needs Improvement grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(76.21);
+
+    //assert
+    expect(grade).toBe("N");
+
+  });
+  
+  it('When user inputs a float 74.99 and below, then the function should return an "D"/Did Not Meet Expectation grade', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(73.21);
+
+    //assert
+    expect(grade).toBe("D");
+
+  });
+  
+  it('When user inputs a string, then the function should return an error message', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier("95");
+
+    //assert
+    expect(grade).toBe("Cannot be non-integer");
+
+  });
+
+  it('When user inputs an empty space, then the function should return an error message', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(" ");
+
+    //assert
+    expect(grade).toBe("Cannot be non-integer");
+
+  });
+
+
+  it('When user inputs a character, then the function should return an error message', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier("A");
+
+    //assert
+    expect(grade).toBe("Cannot be non-integer");
+
+  });
+
+  it('When user inputs a unique character, then the function should return an error message', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier("`");
+
+    //assert
+    expect(grade).toBe("Cannot be non-integer");
+
+  });
+
+  it('When user inputs nothing, then the function should return an error message', () => {
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier();
+
+    //assert
+    expect(grade).toBe("Cannot be null");
+  });
+  
+  it('When user inputs a negative number, then the function should return an error message', ()=>{
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(-1);
+
+    //assert
+    expect(grade).toBe("Cannot be negative");
+    
+  });
+  it('When user inputs a number above 100, then the function should return an error message', ()=>{
+    //arrange
+    var grade;
+    
+    //act
+    grade = gradeClassifier(100.1);
+
+    //assert
+    expect(grade).toBe("Exceeded grade limit");
+    
   });
 });
